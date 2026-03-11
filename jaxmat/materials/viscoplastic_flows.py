f52795c6470b4f52661b4e8f58ec37c3a9c17da9
Jack S. Hale <mail@jackhale.co.uk>
2026-03-11 13:32:25 +0100

    More ruffing

--- jaxmat/materials/viscoplastic_flows.py
+++ jaxmat/materials/viscoplastic_flows.py
@@ -58,7 +58,8 @@ class AbstractKinematicHardening(eqx.Module):
 
     @abstractmethod
     def __call__(self, X, *args):
-        r"""Returns the expression for $\dot{\bX}$ as a function of the backstress $\bX$ and, possibly, other variables."""
+        r"""Returns the expression for $\dot{\bX}$ as a function of the
+        backstress $\bX$ and any other variables."""
         pass
 
     def sig_eff(self, sig, X):
@@ -75,7 +76,8 @@ class LinearKinematicHardening(eqx.Module):
     .. admonition:: References
         :class: seealso
 
-        Prager, W. (1956). A new method of analyzing stresses and strains in work-hardening plastic solids.
+        Prager, W. (1956). A new method of analyzing stresses and strains in work-hardening
+        plastic solids.
     """
 
     H: float = enforce_dtype()
@@ -84,7 +86,8 @@ class LinearKinematicHardening(eqx.Module):
 
     @abstractmethod
     def __call__(self, eps_dot):
-        r"""Returns the expression for $\dot{\bX}$ as a function of the backstress $\bX$ and, possibly, other variables."""
+        r"""Returns the expression for $\dot{\bX}$ as a function of the
+        backstress $\bX$ and any other variables."""
         return 2 / 3 * self.H * eps_dot
 
     def sig_eff(self, sig, X):
